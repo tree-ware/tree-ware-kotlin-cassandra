f77709c9f0dce795dbd4df587fef9398a692d840
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2020-12-31 15:59:22 -0800

    Use test helper

--- src/test/kotlin/org/tree_ware/cassandra/schema/map/AddressBookSchemaMapAsModelTests.kt
+++ src/test/kotlin/org/tree_ware/cassandra/schema/map/AddressBookSchemaMapAsModelTests.kt
@@ -1,12 +1,8 @@
 package org.tree_ware.cassandra.schema.map
 
-import org.tree_ware.model.codec.encodeJson
-import org.tree_ware.model.getFileReader
+import org.tree_ware.model.assertMatchesJson
 import org.tree_ware.schema.core.validate
-import java.io.StringWriter
 import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
 class AddressBookSchemaMapAsModelTests {
@@ -21,25 +17,6 @@ class AddressBookSchemaMapAsModelTests {
         assertTrue(schemaMapErrors.isEmpty())
 
         val mapModel = asModel("test", schemaMap)
-
-        // Encode the map model to JSON and compare with expected JSON
-        val jsonWriter = StringWriter()
-        val isEncoded = try {
-            encodeJson(mapModel, DbSchemaMapAuxEncoder(), jsonWriter, true)
-        } catch (e: Throwable) {
-            e.printStackTrace()
-            println("Encoded so far:")
-            println(jsonWriter.toString())
-            println("End of encoded")
-            false
-        }
-        assertTrue(isEncoded)
-
-        val expectedFileReader = getFileReader("map/address_book_db_schema_map_model.json")
-        assertNotNull(expectedFileReader)
-        val expected = expectedFileReader.readText()
-
-        val actual = jsonWriter.toString()
-        assertEquals(expected, actual)
+        assertMatchesJson(mapModel, DbSchemaMapAuxEncoder(), "map/address_book_db_schema_map_model.json")
     }
 }
