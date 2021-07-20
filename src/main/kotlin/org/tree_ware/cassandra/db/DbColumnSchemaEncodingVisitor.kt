a1b12eb88c62584e45449efdf45208de07b79133
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2021-07-20 14:16:29 -0700

    Use the renamed & relocated traversal enum

--- src/main/kotlin/org/tree_ware/cassandra/db/DbColumnSchemaEncodingVisitor.kt
+++ src/main/kotlin/org/tree_ware/cassandra/db/DbColumnSchemaEncodingVisitor.kt
@@ -6,6 +6,7 @@ import com.datastax.oss.driver.api.core.type.DataTypes
 import com.datastax.oss.driver.api.querybuilder.schema.CreateType
 import com.datastax.oss.driver.internal.core.type.UserDefinedTypeBuilder
 import org.tree_ware.cassandra.schema.map.KeyType
+import org.tree_ware.common.traversal.TraversalAction
 import org.tree_ware.schema.core.*
 import org.tree_ware.schema.visitor.AbstractSchemaVisitor
 import java.util.*
@@ -15,7 +16,7 @@ data class ColumnSchema(val name: CqlIdentifier, val keyType: KeyType?, val data
 class DbColumnSchemaEncodingVisitor(
     private val keyspaceName: String,
     private val createTypes: SortedMap<String, CreateType>
-) : AbstractSchemaVisitor<SchemaTraversalAction>(SchemaTraversalAction.CONTINUE) {
+) : AbstractSchemaVisitor<TraversalAction>(TraversalAction.CONTINUE) {
     val columns: List<ColumnSchema> get() = _columns
     private val _columns = mutableListOf<ColumnSchema>()
 
@@ -39,31 +40,31 @@ class DbColumnSchemaEncodingVisitor(
 
     // Fields
 
-    override fun visit(field: FieldSchema): SchemaTraversalAction {
+    override fun visit(field: FieldSchema): TraversalAction {
         nameParts.add(field.name)
-        return SchemaTraversalAction.CONTINUE
+        return TraversalAction.CONTINUE
     }
 
     override fun leave(field: FieldSchema) {
         nameParts.removeAt(nameParts.lastIndex)
     }
 
-    override fun visit(primitiveField: PrimitiveFieldSchema): SchemaTraversalAction {
+    override fun visit(primitiveField: PrimitiveFieldSchema): TraversalAction {
         addColumn(treeWareToCqlDataType(primitiveField.primitive), primitiveField.multiplicity.isList())
-        return SchemaTraversalAction.CONTINUE
+        return TraversalAction.CONTINUE
     }
 
-    override fun visit(aliasField: AliasFieldSchema): SchemaTraversalAction {
+    override fun visit(aliasField: AliasFieldSchema): TraversalAction {
         addColumn(treeWareToCqlDataType(aliasField.resolvedAlias.primitive), aliasField.multiplicity.isList())
-        return SchemaTraversalAction.CONTINUE
+        return TraversalAction.CONTINUE
     }
 
-    override fun visit(enumerationField: EnumerationFieldSchema): SchemaTraversalAction {
+    override fun visit(enumerationField: EnumerationFieldSchema): TraversalAction {
         addColumn(DataTypes.TEXT, enumerationField.multiplicity.isList())
-        return SchemaTraversalAction.CONTINUE
+        return TraversalAction.CONTINUE
     }
 
-    override fun visit(associationField: AssociationFieldSchema): SchemaTraversalAction {
+    override fun visit(associationField: AssociationFieldSchema): TraversalAction {
         val typeName = getDbAssociationTypeName(associationField)
         if (!createTypes.containsKey(typeName)) {
             createTypes[typeName] = encodeCreateDbAssociationType(keyspaceName, associationField, createTypes)
@@ -75,11 +76,11 @@ class DbColumnSchemaEncodingVisitor(
             .withField("dummy", DataTypes.INT)
             .build()
         addColumn(dataType, associationField.multiplicity.isList())
-        return SchemaTraversalAction.CONTINUE
+        return TraversalAction.CONTINUE
     }
 
-    override fun visit(compositionField: CompositionFieldSchema): SchemaTraversalAction {
-        if (compositionField.multiplicity.isList()) return SchemaTraversalAction.ABORT_SUB_TREE
+    override fun visit(compositionField: CompositionFieldSchema): TraversalAction {
+        if (compositionField.multiplicity.isList()) return TraversalAction.ABORT_SUB_TREE
         // TODO(deepak-nulu): visitor-flag to traverse or not traverse the resolvedEntity
         return compositionField.resolvedEntity.traverse(this)
     }
