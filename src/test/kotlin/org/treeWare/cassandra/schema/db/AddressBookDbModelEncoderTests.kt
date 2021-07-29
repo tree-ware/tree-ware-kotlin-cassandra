a2772a4aae5feb2aba54ddcb4f253be9c4249483
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2021-07-28 17:36:02 -0700

    Simplify the JSON representation for models

--- src/test/kotlin/org/treeWare/cassandra/schema/db/AddressBookDbModelEncoderTests.kt
+++ src/test/kotlin/org/treeWare/cassandra/schema/db/AddressBookDbModelEncoderTests.kt
@@ -5,6 +5,7 @@ import org.treeWare.cassandra.schema.map.asModel
 import org.treeWare.cassandra.schema.map.newAddressBookSchema
 import org.treeWare.cassandra.schema.map.newAddressBookSchemaMap
 import org.treeWare.cassandra.schema.map.validate
+import org.treeWare.model.assertMatchesJson
 import org.treeWare.model.getFileReader
 import org.treeWare.model.getModel
 import org.treeWare.schema.core.validate
@@ -28,6 +29,7 @@ class AddressBookDbModelEncoderTests {
 
         val dbSchemaMap = asModel(ENVIRONMENT, schemaMap)
         val data = getModel<Unit>(schema, "db/address_book_write_request.json")
+        assertMatchesJson(data, null, "db/address_book_write_request.json")
 
         val cqlFileReader = getFileReader("db/address_book_db_data_cql.txt")
         assertNotNull(cqlFileReader)
